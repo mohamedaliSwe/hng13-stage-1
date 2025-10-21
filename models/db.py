4720b3e6e75a1b0018f7a026f77fbaf746299e2b
mohamedaliSwe <mohammedcali@proton.me>
2025-10-22 02:32:21 +0300

    Mongodb URI correction

--- models/db.py
+++ models/db.py
@@ -9,6 +9,6 @@ MONGO_PORT = config("MONGO_PORT", cast=int)
 MONGO_DB = config("MONGO_DB")
 
 client = MongoClient(
-    f"mongodb://{MONGO_USERNAME}:{MONGO_PASSWORD}@{MONGO_HOST}:{MONGO_PORT}/{MONGO_DB}")
+    f"mongodb://{MONGO_USERNAME}:{MONGO_PASSWORD}@[{MONGO_HOST}]:{MONGO_PORT}/{MONGO_DB}")
 db = client[MONGO_DB]
 string_collection = db['text']
