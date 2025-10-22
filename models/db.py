c733f9a21b2f30bad8566ff32a64c227431cbb81
mohamedaliSwe <mohammedcali@proton.me>
2025-10-22 03:40:38 +0300

    New mongo database connection

--- models/db.py
+++ models/db.py
@@ -2,12 +2,6 @@ from decouple import config
 from pymongo import MongoClient
 
 
-MONGO_USERNAME = config("MONGO_USERNAME")
-MONGO_PASSWORD = config("MONGO_PASSWORD")
-MONGO_HOST = config("MONGO_HOST")
-MONGO_PORT = config("MONGO_PORT", cast=int)
-MONGO_DB = config("MONGO_DB")
-
 client = MongoClient(config("MONGO_URL"))
-db = client[MONGO_DB]
+db = client.text_analyzer_db
 string_collection = db['text']
